0ddfe49a28ad80d8ed6c08992c6c6afbccbc98a2
venkatesh <76477406+venkat18-pro@users.noreply.github.com>
2021-11-17 03:04:55 -0500

    Update WebElementClassDemo.java

--- WebElementClassDemo.java
+++ WebElementClassDemo.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebElementClassDemo {
 
 	public static void main(String[] args) {
-		//Launch the browser.  
+		//Lunch the browser.  
 		System.setProperty("webdriver.chrome.driver", 
 				"C:\\Users\\ELCOT\\eclipse-workspace\\SeleniumProject\\Drivers\\chromedriver.exe");
 		//create the upcasting refName.
